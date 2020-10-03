3e008d1f1c99687c9c20f5dfbaf5cabe7bc78b73
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2020-10-03 18:19:04 +0300

    Perform factorial calculations

--- RIvanvosCalculator/CalculatorLogic.cs
+++ RIvanvosCalculator/CalculatorLogic.cs
@@ -1,18 +1,24 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace RIvanvosCalculator
 {
     class CalculatorLogic
     {
+
+        private static readonly Regex FACTORTIAL_TOKEN = new Regex(@"(\d+!|\(.+\)!)");
         private string expressionCurrent { get; set; } = "";
+
         public CalculatorLogic()
         {
+            this.expressionCurrent = "";
         }
 
         // Copied over from https://stackoverflow.com/a/6052679
-        // It would be nice to hear in the next upcoming lectures on how to properly do this in C#.
-        public static float Evaluate(string expression)
+        // It would be nice to hear in the next upcoming lectures on what is actually happening here.
+        private static float Evaluate(string expression)
         {
             System.Data.DataTable table = new System.Data.DataTable();
             table.Columns.Add("expression", string.Empty.GetType(), expression);
@@ -36,7 +42,10 @@ namespace RIvanvosCalculator
             string result = "";
             try
             {
-                result = CalculatorLogic.Evaluate(this.expressionCurrent).ToString();
+                // Explicitly handle factorials
+                string parsed_result = EvaluateUnknownFactorials(this.expressionCurrent);
+
+                result = CalculatorLogic.Evaluate(parsed_result).ToString();
             }
             catch (Exception)
             {
@@ -44,7 +53,8 @@ namespace RIvanvosCalculator
             }
 
             // By returning the result and cleaning the inner memory is how we 
-            // achieve the lazy displaying of the result.
+            // achieve the lazy displaying of the result while still "clearing" 
+            // the screen once new numbers get pressed
             this.clearCurrent();
             return result;
         }
@@ -57,6 +67,40 @@ namespace RIvanvosCalculator
         {
             this.expressionCurrent += appendable;
             return this.expressionCurrent;
+        }
+
+        /**
+         * Find every factorial in a substring and evaluate it. Works recursevly.
+         * Once the factorial has been evaluated -> replace the expression in 
+         * the input string with the calculated value, return the altered string.
+         * 
+         */
+        private static string EvaluateUnknownFactorials(string expression)
+        { 
+            Match match = FACTORTIAL_TOKEN.Match(expression);
+            CaptureCollection captures = match.Captures;
+            foreach (Capture capture in captures)
+            {
+                // remove the '!'
+                string group_substring = capture.Value.Substring(0, capture.Value.Length - 1); 
+                
+                if (group_substring.StartsWith("(") && group_substring.EndsWith(")"))
+                {
+                    string subresult = EvaluateUnknownFactorials(group_substring);
+                    int subvalue = (int)CalculatorLogic.Evaluate(subresult);
+                    group_substring = group_substring.Replace(group_substring, subvalue.ToString());
+                }
+
+                int localResult = (int)CalculatorLogic.Evaluate(group_substring);
+                localResult = CalculatorLogic.Factorial(localResult);
+                expression = expression.Replace(capture.Value, localResult.ToString());
+            }
+            return expression;
+        }
+
+        private static int Factorial(int i)
+        {
+            return i <= 1 ? i : i * (i - 1);
         }
 
     }
